6d4b63b3c3e68440d11c5d5557238466e2ea9d66
i-guerrero <isaacguerrero@pursuit.org>
2022-09-12 22:17:47 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let url =
+  "https://newsapi.org/v2/everything?" +
+  "q=Apple&" +
+  "from=2022-09-10&" +
+  "sortBy=popularity&" +
+  "apiKey=1150fa9d1b774b93b2f3b55abe77fb16";
+
+let req = new Request(url);
+
+fetch(req)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    console.log(data.articles);
+  });
---
+++ proposal.md
@@ -0,0 +1,5 @@
+# Climate News Aggregator
+
+## User Stories
+
+As an environmentally concerned citizen, I want to be able to see top climate change articles, so that I can see and read news articles from a certain date and about specific topics.
