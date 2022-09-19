7918274fb04af3eb6a93fd6d284c086583e6b8e3
i-guerrero <isaacguerrero@pursuit.org>
2022-09-18 21:56:32 -0400

    Add articles to page on submit

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
+const main = document.querySelector("main");
+
 let url =
   "https://newsapi.org/v2/everything?" +
-  "q=Apple&" +
+  "q=climate&" +
   "from=2022-09-10&" +
   "sortBy=popularity&" +
   "apiKey=1150fa9d1b774b93b2f3b55abe77fb16";
 
 let req = new Request(url);
 
-fetch(req)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    console.log(data.articles);
-  });
+function displayData(data) {
+  for (let i = 0; i < 10; i++) {
+    // Get article
+    const article = data.articles[i];
 
-const form = document.querySelector("main form");
+    // Create article HTML skeleton
+    const articleEle = document.createElement("article");
+    const a = document.createElement("a");
+    // Heading
+    const h2Title = document.createElement("h2");
+    const pAuthor = document.createElement("p");
+    const img = document.createElement("img");
+    const pDescription = document.createElement("p");
+    // Content
+    const pContent = document.createElement("p");
+    // Footer
+    const pSource = document.createElement("p");
+    const pReadMore = document.createElement("p");
+
+    // Add article content to skelton
+    h2Title.innerText = article.title;
+    pAuthor.innerText = article.author;
+    img.setAttribute("src", article.urlToImage);
+    img.setAttribute("alt", article.title);
+    pDescription.innerText = article.description;
+    pContent.innerText = article.content;
+    pSource.innerText = article.source.name;
+    a.setAttribute("href", article.url);
+    pReadMore.innerText = "Read More";
+
+    main.append(articleEle);
+    articleEle.append(a);
+    a.append(h2Title, pAuthor, img, pDescription, pContent, pSource, pReadMore);
+    console.log(h2Title);
+  }
+}
+
+const form = document.querySelector("form");
 
 // Create Event listener
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  fetch(req)
+    .then((response) => response.json())
+    .then(displayData);
 });
